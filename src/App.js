2df08ee65e2c15fba713205443ea334bbf291f46
Bolaji <rotibi.m@husky.neu.edu>
2019-08-18 12:51:24 +0100

    Transferred files from my test repository to this official user space GitHub Pages directory

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navigation from './views/Navigation.js';
+import Content from './views/Content.js'
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Navigation />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+
+function Showcase() {
+    return (
+        <div className='Showcase'>
+        </div>
+    );
+};
+
+export default Showcase;
