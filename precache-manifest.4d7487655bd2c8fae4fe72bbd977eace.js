a68981cd9e265376ace9444e3edddebe289601d3
Bolaji <rotibi.m@husky.neu.edu>
2019-09-13 15:20:35 -0400

    Updates

---
+++ precache-manifest.4d7487655bd2c8fae4fe72bbd977eace.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5134da064847abbbb3a6a9b9b80751a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9a19c987f170f2f61c8",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "de7031c7511f83bf019a",
+    "url": "/static/css/main.80f71693.chunk.css"
+  },
+  {
+    "revision": "e9a19c987f170f2f61c8",
+    "url": "/static/js/2.8b7525ef.chunk.js"
+  },
+  {
+    "revision": "de7031c7511f83bf019a",
+    "url": "/static/js/main.fc0bf5a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
